f914689b8b520747149055462d02222bde5586d9
M. Endyarroziqin <endy@millennia-solusi.id>
2023-10-27 06:32:59 +0700

    Section 03 - Chapter 24 ~ Create New Controller

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,35 @@
+using learn.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://picsum.photos/200/300"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://picsum.photos/200/300"
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
