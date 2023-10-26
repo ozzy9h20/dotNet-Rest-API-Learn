71043d8620f7215da513474c862abd98a794dc26
M. Endyarroziqin <endy@millennia-solusi.id>
2023-10-27 00:31:46 +0700

    chore: vscode c# extension

--- Data/DefaultDbContext.cs
+++ Data/DefaultDbContext.cs
@@ -3,14 +3,13 @@ using learn.Models.Domain;
 
 namespace learn.Data
 {
-  public class DefaultDbContext : DbContext
-  {
-    public DefaultDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    public class DefaultDbContext : DbContext
     {
-    }
+        public DefaultDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions) { }
 
-    public DbSet<Difficulty> Difficulties { get; set; }
-    public DbSet<Region> Regions { get; set; }
-    public DbSet<Walk> Walks { get; set; }
-  }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
 }
