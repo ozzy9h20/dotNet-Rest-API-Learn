a70bd2e505234f45a1630f39950bafe171df69d8
M. Endyarroziqin <endy@millennia-solusi.id>
2023-10-27 07:40:48 +0700

    Section 03 - Chapter 29 ~ Create Region Action Method

--- Controllers/RegionsController.cs
+++ Controllers/RegionsController.cs
@@ -71,5 +71,34 @@ namespace learn.Controllers
             // Return DTO Back to client
             return Ok(regionDto);
         }
+
+        // POST TO CREATE NEW REGION
+        // POST: https://localhost:7027/api/Regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map/Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // Use Domain Model to Create Region
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            // Map Domain Model Back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+        }
     }
 }
