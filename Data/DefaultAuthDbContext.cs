cb539dc5e69dc3b82cb5d563199d0e864a018d10
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-18 17:16:52 +0700

    Section 08 - Chapter 60 ~ Seeding Roles

--- Data/DefaultAuthDbContext.cs
+++ Data/DefaultAuthDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,5 +8,33 @@ namespace learn.Data
     {
         public DefaultAuthDbContext(DbContextOptions<DefaultAuthDbContext> options)
             : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "bea16ac0-09fd-451f-914b-6b83266320ee";
+            var writerRoleId = "ac7dd438-d683-492f-828c-0f4bd48d4297";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper(),
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
