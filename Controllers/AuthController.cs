b7802fe93f0c4a726140169cac8bc0d7f1a2b0f6
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-18 17:53:47 +0700

    Section 08 - Chapter 63 ~ Create Auth Controller and Register Action Method

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+using learn.Models.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+            var identityResult = await userManager.CreateAsync(
+                identityUser,
+                registerRequestDto.Password
+            );
+
+            if (identityResult.Succeeded)
+            {
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(
+                        identityUser,
+                        registerRequestDto.Roles
+                    );
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered, Please login.");
+                    }
+                }
+            }
+
+            return BadRequest("Something went wrong");
+        }
+    }
+}
---
+++ Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace learn.Models.DTO
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public string[] Roles { get; set; }
+    }
+}
