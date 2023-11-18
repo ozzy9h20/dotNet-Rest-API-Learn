0618578e235066160c2c930e3c384c81cec962dc
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-18 18:04:12 +0700

    Section 08 - Chapter 64 ~ Create Login Action Method

---
+++ Models/DTO/LoginRequestDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace learn.Models.DTO
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
