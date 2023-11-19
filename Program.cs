d3604bf952a02afe7f1f3544f05cc09e1b71ffa3
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-19 14:25:20 +0700

    Section 09 - Chapter 74 ~ Serving Static Files Through ASPNET Core Web API

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
+using System.Net.NetworkInformation;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -112,6 +114,16 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseStaticFiles(
+    new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "Images")
+        ),
+        RequestPath = "/Images"
+    }
+);
 
 app.MapControllers();
 
